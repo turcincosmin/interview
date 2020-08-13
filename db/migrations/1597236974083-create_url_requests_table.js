619bde25c28429c36856fb43f87bc5be95c745fa
Cosmin Turcin <cosmin.turcin@udevoffice.ro>
2020-08-13 09:50:35 +0300

    feat: add migration and seed to populate requets table

---
+++ db/migrations/1597236974083-create_url_requests_table.js
@@ -0,0 +1,38 @@
+require("../../models");
+
+/**
+ * Make any changes you need to make to the database here
+ */
+async function up() {
+  // Write migration here
+  const requests = [
+    {
+      id: 1,
+      url: "https://proxify.io",
+      status: "NEW",
+      http_code: null,
+      updated_at: new Date(),
+      created_at: new Date(),
+    },
+    {
+      id: 2,
+      url: "https://proxify.io",
+      status: "NEW",
+      http_code: null,
+      updated_at: new Date(),
+      created_at: new Date(),
+    },
+  ];
+
+  await this("requests").collection.insertMany(requests);
+}
+
+/**
+ * Make any changes that UNDO the up function side effects here (if possible)
+ */
+async function down() {
+  // Write migration here
+  await this("requests").collection.drop();
+}
+
+module.exports = { up, down };
