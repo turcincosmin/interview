9104e5d0642f29e30c1afe626048b1bc4b1c718e
Cosmin Turcin <cosmin.turcin@udevoffice.ro>
2020-08-13 09:51:02 +0300

    feat: add post endpoint to add new request

---
+++ features/addRequest/routes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const addRequest = require("./repository/addRequest");
+
+router.post("/add-request", async (req, res) => {
+  const { url } = req.body;
+  try {
+    await addRequest(url);
+    res.json({
+      success: true,
+      message: "Successfully added a new request entry",
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: "Something went wrong",
+    });
+  }
+});
+
+module.exports = router;
