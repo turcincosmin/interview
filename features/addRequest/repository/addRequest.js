ad84d705c984f860154f4b7af2140351bf76bd2d
Cosmin Turcin <cosmin.turcin@udevoffice.ro>
2020-08-13 09:51:41 +0300

    feat: create add request query to insert new request url

---
+++ features/addRequest/repository/addRequest.js
@@ -0,0 +1,22 @@
+const { Requests } = require("../../../models");
+const addRequests = async (url) => {
+  if (!url) {
+    throw new Error("URL param is missing");
+  }
+  try {
+    const requestsLength = await Requests.count();
+    const newRequest = Requests.collection.insertOne({
+      url,
+      id: requestsLength + 1,
+      status: "NEW",
+      http_code: null,
+      updated_at: new Date(),
+      created_at: new Date(),
+    });
+    return newRequest;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+module.exports = addRequests;
